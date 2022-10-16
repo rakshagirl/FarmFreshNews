d835fbc9449506a4b8296322a6c07bbe283223bb
Varshinee Sreekanth <varshinee@utexas.edu>
2022-10-16 10:56:15 -0500

    fixed up frontend

--- agroplus/js/App.js
+++ agroplus/js/App.js
@@ -5,14 +5,15 @@ const port = 3100;
 
 // Client Connection - Start
 const { Client } = require("pg");
-const DATABASE_URL="postgresql://sairaja:w1vrDBPKPOP7CRO9IPgfIA@free-tier14.aws-us-east-1.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&options=--cluster%3Dbitter-marten-5761"
+const DATABASE_URL="[REDACTED]"
 // Client Connection - End
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  init_connection(res);
-  // sentiment_analysis("I am so mad right now");
-  res.end('Hello World');
+  rows = init_connection(res);
+  for (let i=0; i<rows.length;i++) {
+    sentiment_analysis(rows[i]);
+  }
 });
 
 server.listen(port, hostname, () => {
@@ -24,7 +25,7 @@ function init_connection(res){
     (async () => {
     await client.connect();
     try {
-        const results = await client.query("SELECT * FROM test1;");
+        const results = await client.query("SELECT * FROM FarmFresh;");
         console.log(results.rows);
     } catch (err) {
         console.error("error executing query:", err);
@@ -32,17 +33,12 @@ function init_connection(res){
         client.end();
     }
     })();
+    return results.rows();
 }
 
 async function sentiment_analysis(text) {
   // Imports the Google Cloud client library
   const language = require('@google-cloud/language');
-  // const keyfile = require("../../../../../virtual-rarity-365619-febe11feac95.json")
-
-  const config = {
-	  projectId: "farm-fresh-news",
-	  keyFilename: "../../../../../farm-fresh-news-b2f156736747.json"
-	};
 
   // Instantiates a client
   const client = new language.LanguageServiceClient();
@@ -60,4 +56,3 @@ async function sentiment_analysis(text) {
   console.log(`Sentiment score: ${sentiment.score}`);
   console.log(`Sentiment magnitude: ${sentiment.magnitude}`);
 }
-
